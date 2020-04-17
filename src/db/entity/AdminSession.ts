257c3b3db8a3a5e4e8b66417ad19481608afd02d
AcrylicShrimp <led789zxpp@naver.com>
2020-04-17 20:21:41 +0900

    FEAT: Define AdminSession entity.

---
+++ src/db/entity/AdminSession.ts
@@ -0,0 +1,29 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	OneToOne,
+	JoinColumn
+} from 'typeorm';
+
+import { Admin } from './Admin';
+
+@Entity()
+export class AdminSession {
+	@PrimaryGeneratedColumn()
+	id!: number;
+
+	@Column({ length: 256, unique: true, nullable: false })
+	token!: string;
+
+	@OneToOne(() => Admin)
+	@JoinColumn()
+	user!: Admin;
+
+	@Column({ nullable: false })
+	usedAt!: Date;
+
+	@CreateDateColumn()
+	createdAt!: Date;
+}
