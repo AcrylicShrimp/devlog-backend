5cb468e6eb5bde3750aedffa3b85d0b4e2c2fde3
AcrylicShrimp <led789zxpp@naver.com>
2020-04-19 11:46:16 +0900

    FEAT: Implement category creation endpoint.

--- src/admin/admin.category.controller.ts
+++ src/admin/admin.category.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, UseGuards, Get } from '@nestjs/common';
+import {
+	Controller,
+	UseGuards,
+	Get,
+	Post,
+	Body,
+	BadRequestException,
+	ConflictException
+} from '@nestjs/common';
+import validator from 'validator';
 
 import { AdminGuard } from './admin.guard';
 
 import { DBConnService } from '../db/db.conn.service';
 
 import { Category } from '../db/entity/Category';
+import { QueryFailedError } from 'typeorm';
 
 @Controller()
 @UseGuards(AdminGuard)
@@ -18,5 +28,35 @@ export class AdminCategoryController {
 				name: 'ASC'
 			}
 		});
+	}
+
+	@Post('admin/categories')
+	async newCategory(
+		@Body('name') name: string,
+		@Body('desc') desc: string
+	): Promise<void> {
+		if (!name || !(name = name.trim()))
+			throw new BadRequestException('name required');
+
+		if (!validator.isLength(name, { max: 32 }))
+			throw new BadRequestException('name too long');
+
+		if (!desc || !(desc = desc.trim())) desc = '';
+
+		if (!validator.isLength(desc, { max: 256 }))
+			throw new BadRequestException('desc too long');
+
+		const category = new Category();
+		category.name = name;
+		category.description = desc;
+
+		try {
+			await this.conn.conn.manager.save(category);
+		} catch (err) {
+			if (err instanceof QueryFailedError)
+				throw new ConflictException('name already exists');
+
+			throw err;
+		}
 	}
 }
