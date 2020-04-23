9a23c77598d0fb6fb8107116b327a97ca7a73fac
AcrylicShrimp <led789zxpp@naver.com>
2020-04-23 16:22:37 +0900

    FEAT: Add busboy middleware pass.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { Request, Response, NextFunction } from 'express';
+
 import helmet from 'helmet';
 import cors from 'cors';
+import busboy from 'connect-busboy';
 
 import { AppModule } from './app.module';
 
@@ -15,6 +18,17 @@ import { AppModule } from './app.module';
 			methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 		})
 	);
+	app.use(busboy());
+	app.use((req: Request, res: Response, next: NextFunction) => {
+		req.busboy.on(
+			'field',
+			(key, value, keyTruncated, valueTruncated) =>
+				(req.body[key] = value)
+		);
+		req.pipe(req.busboy);
+
+		next();
+	});
 
 	await app.listen(8000, '0.0.0.0');
 })();
