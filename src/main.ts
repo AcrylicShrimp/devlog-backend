5121e5f3d2d7c8c7e69a33ec3613c438987dfdc9
AcrylicShrimp <led789zxpp@naver.com>
2020-04-29 09:09:07 +0900

    FIX: Remove a unneeded busboy middleware.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+
 import { NestFactory } from '@nestjs/core';
 import { Request, Response, NextFunction } from 'express';
 
@@ -8,6 +10,8 @@ import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 (async () => {
+	dotenv.config();
+
 	const app = await NestFactory.create(AppModule);
 
 	app.use(helmet());
@@ -19,16 +23,6 @@ import { AppModule } from './app.module';
 		})
 	);
 	app.use(busboy());
-	app.use((req: Request, res: Response, next: NextFunction) => {
-		req.busboy.on(
-			'field',
-			(key, value, keyTruncated, valueTruncated) =>
-				(req.body[key] = value)
-		);
-		req.pipe(req.busboy);
-
-		next();
-	});
 
 	await app.listen(8000, '0.0.0.0');
 })();
