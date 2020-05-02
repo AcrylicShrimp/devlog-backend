94f952d0a4e98dd0bcd3017c24dbdcd60aee77b3
AcrylicShrimp <led789zxpp@naver.com>
2020-05-02 17:07:58 +0900

    CHORE: Create migration.

---
+++ src/db/migration/1588406760635-ModifyPostItem.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ModifyPostItem1588406760635 implements MigrationInterface {
+    name = 'ModifyPostItem1588406760635'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `admin_session` DROP FOREIGN KEY `FK_b41305caf8ef2f3847957746a2f`", undefined);
+        await queryRunner.query("ALTER TABLE `admin_session` CHANGE `userId` `userId` int NULL", undefined);
+        await queryRunner.query("ALTER TABLE `category` CHANGE `description` `description` varchar(256) NULL", undefined);
+        await queryRunner.query("ALTER TABLE `post_item_image` DROP FOREIGN KEY `FK_a0e437b6ad0b098a1f22357c815`", undefined);
+        await queryRunner.query("ALTER TABLE `post_item_image` CHANGE `postId` `postId` int NULL", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` DROP FOREIGN KEY `FK_583829a77a7b675981de33ecae2`", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` DROP COLUMN `contentPreview`", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` ADD `contentPreview` varchar(256) NULL", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` CHANGE `content` `content` mediumtext NULL", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` CHANGE `htmlContent` `htmlContent` mediumtext NULL", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` CHANGE `categoryId` `categoryId` int NULL", undefined);
+        await queryRunner.query("ALTER TABLE `admin_session` ADD CONSTRAINT `FK_b41305caf8ef2f3847957746a2f` FOREIGN KEY (`userId`) REFERENCES `admin`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
+        await queryRunner.query("ALTER TABLE `post_item_image` ADD CONSTRAINT `FK_a0e437b6ad0b098a1f22357c815` FOREIGN KEY (`postId`) REFERENCES `post_item`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` ADD CONSTRAINT `FK_583829a77a7b675981de33ecae2` FOREIGN KEY (`categoryId`) REFERENCES `category`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `post_item` DROP FOREIGN KEY `FK_583829a77a7b675981de33ecae2`", undefined);
+        await queryRunner.query("ALTER TABLE `post_item_image` DROP FOREIGN KEY `FK_a0e437b6ad0b098a1f22357c815`", undefined);
+        await queryRunner.query("ALTER TABLE `admin_session` DROP FOREIGN KEY `FK_b41305caf8ef2f3847957746a2f`", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` CHANGE `categoryId` `categoryId` int NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` CHANGE `htmlContent` `htmlContent` mediumtext NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` CHANGE `content` `content` mediumtext NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` DROP COLUMN `contentPreview`", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` ADD `contentPreview` varchar(128) NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `post_item` ADD CONSTRAINT `FK_583829a77a7b675981de33ecae2` FOREIGN KEY (`categoryId`) REFERENCES `category`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
+        await queryRunner.query("ALTER TABLE `post_item_image` CHANGE `postId` `postId` int NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `post_item_image` ADD CONSTRAINT `FK_a0e437b6ad0b098a1f22357c815` FOREIGN KEY (`postId`) REFERENCES `post_item`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
+        await queryRunner.query("ALTER TABLE `category` CHANGE `description` `description` varchar(256) NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `admin_session` CHANGE `userId` `userId` int NULL DEFAULT 'NULL'", undefined);
+        await queryRunner.query("ALTER TABLE `admin_session` ADD CONSTRAINT `FK_b41305caf8ef2f3847957746a2f` FOREIGN KEY (`userId`) REFERENCES `admin`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION", undefined);
+    }
+
+}
