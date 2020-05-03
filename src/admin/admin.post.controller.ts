fcbd2a879b3406305e6a3aee467c579c199fad9c
AcrylicShrimp <led789zxpp@naver.com>
2020-05-04 03:17:51 +0900

    FEAT: Add a post deletion endpoint.

--- src/admin/admin.post.controller.ts
+++ src/admin/admin.post.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, UseGuards, Req } from '@nestjs/common';
-import { Get, Post } from '@nestjs/common';
+import { Controller, UseGuards, Req, NotFoundException } from '@nestjs/common';
+import { Get, Post, Delete } from '@nestjs/common';
+import { Param } from '@nestjs/common';
 import { BadRequestException, ConflictException } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
 import { encode } from 'blurhash';
@@ -393,6 +394,51 @@ export class AdminPostController {
 
 				throw err;
 			}
+		});
+	}
+
+	@Delete('admin/posts/:slug')
+	async deletePost(@Param('slug') slug: string): Promise<void> {
+		if (!slug || !(slug = slug.trim()))
+			throw new BadRequestException('slug required');
+
+		if (!SlugRegex.test(slug)) throw new BadRequestException('bad slug');
+
+		await this.conn.conn.transaction(async (mgr) => {
+			const post = await mgr
+				.createQueryBuilder(PostItem, 'PostItem')
+				.leftJoin('PostItem.images', 'PostItemImage')
+				.where('PostItem.slug = :slug', { slug })
+				.select([
+					'PostItem.id',
+					'PostItem.uuid',
+					'PostItemImage.id',
+					'PostItemImage.uuid'
+				])
+				.getOne();
+
+			if (!post) throw new NotFoundException('no post found');
+
+			for (let index = 0; index < post.images.length; index += 1000)
+				try {
+					await this.s3
+						.deleteObjects({
+							Bucket: process.env.AWS_S3_BUCKET_NAME!,
+							Delete: {
+								Objects: post.images
+									.slice(index, index + 1000)
+									.map((image) => {
+										return {
+											Key: `${post.uuid}/${image.uuid}`
+										};
+									})
+							}
+						})
+						.promise();
+				} catch {}
+
+			mgr.remove(post.images);
+			mgr.remove(post);
 		});
 	}
 }
