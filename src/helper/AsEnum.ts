35d28078f21a133a8fa7487f60b9646d64d61107
AcrylicShrimp <led789zxpp@naver.com>
2020-05-03 17:52:19 +0900

    FEAT: Add a helper function asEnum.

---
+++ src/helper/AsEnum.ts
@@ -0,0 +1,13 @@
+type Not<T> = [T] extends [never] ? unknown : never;
+type Extractable<T, U> = Not<
+	U extends any ? Not<T extends U ? unknown : never> : never
+>;
+
+export function asEnum<
+	E extends Record<keyof E, string | number>,
+	K extends string | number
+>(e: E, k: K & Extractable<E[keyof E], K>): Extract<E[keyof E], K> {
+	if (0 <= Object.values(e).indexOf(k)) return k as any;
+
+	throw new Error(`expected one of ${Object.values(e).join(', ')}`);
+}
