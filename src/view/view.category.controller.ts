041b2ae1d9e14ff6a43efafc15d31054ab088bae
AcrylicShrimp <led789zxpp@naver.com>
2020-05-08 15:33:22 +0900

    FEAT: Add a category listing endpoint.

---
+++ src/view/view.category.controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common';
+
+import { DBConnService } from '../db/db.conn.service';
+
+import { Category } from '../db/entity/Category';
+
+@Controller()
+export class ViewCategoryController {
+	constructor(private conn: DBConnService) {}
+
+	@Get('categories')
+	async listCategories(): Promise<Category[]> {
+		return this.conn.conn.manager.find(Category, {
+			select: ['name', 'description', 'createdAt', 'modifiedAt'],
+			order: {
+				name: 'ASC'
+			}
+		});
+	}
+}
--- src/view/view.module.ts
+++ src/view/view.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { ViewPostController } from './view.post.controller';
+import { ViewCategoryController } from './view.category.controller';
 
 @Module({
-	controllers: [ViewPostController]
+	controllers: [ViewCategoryController, ViewPostController]
 })
 export class ViewModule {}
