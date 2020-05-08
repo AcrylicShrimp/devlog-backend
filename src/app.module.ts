fadd10fd51caf015360dd9b377862350f9348e01
AcrylicShrimp <led789zxpp@naver.com>
2020-05-08 09:38:42 +0900

    FEAT: Add a post module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AdminModule } from './admin/admin.module';
 import { DBModule } from './db/db.module';
+import { PostModule } from './post/post.module';
 
-@Module({ imports: [AdminModule, DBModule] })
+@Module({ imports: [AdminModule, DBModule, PostModule] })
 export class AppModule {}
