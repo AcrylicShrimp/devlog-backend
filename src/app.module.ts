407a7187d08257f0e6762bbf86ee062e676be127
AcrylicShrimp <led789zxpp@naver.com>
2020-07-27 22:11:13 +0900

    FEAT: Add a new way to integrate with the sentry.io.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, Type, DynamicModule } from '@nestjs/common';
+import { SentryModule } from '@ntegral/nestjs-sentry';
+import { LogLevel } from '@sentry/types';
 
 import { AdminModule } from './admin/admin.module';
 import { DBModule } from './db/db.module';
 import { ViewModule } from './view/view.module';
 
-@Module({ imports: [AdminModule, DBModule, ViewModule] })
+const imports: (Type<any> | DynamicModule)[] = [
+	AdminModule,
+	DBModule,
+	ViewModule,
+];
+
+if (process.env.SENTRY_DSN)
+	imports.push(
+		SentryModule.forRoot({
+			dsn: process.env.SENTRY_DSN,
+			debug: false,
+			environment: process.env.NODE_ENV,
+			logLevel: LogLevel.Error,
+		})
+	);
+
+@Module({ imports })
 export class AppModule {}
