654c9bb446229d3d1e8131cb03ea21b81c062a01
AcrylicShrimp <led789zxpp@naver.com>
2020-08-12 16:37:32 +0900

    FEAT: Add integration with elasticsearch.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
+import dotenv from 'dotenv';
+
 import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 import { AdminModule } from './admin/admin.module';
 import { DBModule } from './db/db.module';
 import { ViewModule } from './view/view.module';
 
-@Module({ imports: [AdminModule, DBModule, ViewModule] })
+dotenv.config();
+
+@Module({
+	imports: [
+		AdminModule,
+		DBModule,
+		ViewModule,
+		ElasticsearchModule.register({
+			node: `http://${process.env.ELASTICSEARCH_NODE_HOST}:${process.env.ELASTICSEARCH_NODE_PORT}`,
+		}),
+	],
+})
 export class AppModule {}
