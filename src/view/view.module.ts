426ae9a6c240c9dc5f7ad2575b68ac8d7d7e3fa1
AcrylicShrimp <led789zxpp@naver.com>
2020-08-14 13:08:14 +0900

    FEAT: Add a query string query to provide post searching.

--- src/view/view.module.ts
+++ src/view/view.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 import { ViewPostController } from './view.post.controller';
 import { ViewCategoryController } from './view.category.controller';
 
 @Module({
+	imports: [
+		ElasticsearchModule.register({
+			node: `http://${process.env.ELASTICSEARCH_NODE_HOST}:${process.env.ELASTICSEARCH_NODE_PORT}`,
+		}),
+	],
 	controllers: [ViewCategoryController, ViewPostController],
 })
 export class ViewModule {}
