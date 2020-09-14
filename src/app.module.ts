c372b0520239a34bcb244a4e1a73f39639fdffd5
AcrylicShrimp <led789zxpp@naver.com>
2020-09-14 23:56:03 +0900

    FEAT: Drop the old way to integrate with elaticsearch.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AdminModule } from './admin/admin.module';
 import { DBModule } from './db/db.module';
-import { SchedulerModule } from './scheduler/scheduler.module';
+import { ESModule } from './es/es.module';
 import { ViewModule } from './view/view.module';
 
 @Module({
-	imports: [AdminModule, DBModule, SchedulerModule, ViewModule],
+	imports: [AdminModule, DBModule, ESModule, ViewModule],
 })
 export class AppModule {}
--- src/es/es.module.ts
+++ src/es/es.module.ts
@@ -4,11 +4,6 @@ import {
 	ElasticsearchService,
 } from '@nestjs/elasticsearch';
 
-import { DBConnService } from '../db/db.conn.service';
-
-import { PostItem } from '../db/entity/PostItem';
-import { parseAsText } from '../helper/MDRenderer';
-
 @Module({
 	imports: [
 		ElasticsearchModule.register({
@@ -16,8 +11,8 @@ import { parseAsText } from '../helper/MDRenderer';
 		}),
 	],
 })
-export class SchedulerModule {
-	constructor(private es: ElasticsearchService, private conn: DBConnService) {
+export class ESModule {
+	constructor(es: ElasticsearchService) {
 		(async () => {
 			try {
 				if (!(await es.indices.exists({ index: 'devlog-posts' })).body)
@@ -83,69 +78,11 @@ export class SchedulerModule {
 							},
 						},
 					});
-
-				setInterval(this.syncWithES.bind(this), 1000 * 60);
 			} catch (err) {
 				console.error(
 					`[scheduler] - [INIT] :: An error occurred: ${err}`
 				);
 			}
 		})();
-	}
-
-	private async syncWithES() {
-		try {
-			const posts = await this.conn.conn.manager
-				.createQueryBuilder(PostItem, 'PostItem')
-				.where('PostItem.content IS NOT NULL')
-				.select(['PostItem.id', 'PostItem.slug'])
-				.orderBy('PostItem.createdAt', 'DESC')
-				.getMany();
-
-			for (let index = 0; index < posts.length; ++index) {
-				if (
-					(
-						await this.es.exists({
-							id: posts[index].slug,
-							index: 'devlog-posts',
-						})
-					).body
-				)
-					break;
-
-				const post = await this.conn.conn.manager
-					.createQueryBuilder(PostItem, 'PostItem')
-					.where('PostItem.id = :id', { id: posts[index].id })
-					.andWhere('PostItem.content IS NOT NULL')
-					.leftJoin('PostItem.category', 'Category')
-					.select([
-						'PostItem.accessLevel',
-						'PostItem.title',
-						'PostItem.content',
-						'PostItem.createdAt',
-						'Category.name',
-					])
-					.orderBy('PostItem.createdAt', 'DESC')
-					.getOne();
-
-				if (!post) continue;
-
-				await this.es.create({
-					id: posts[index].slug,
-					index: 'devlog-posts',
-					body: {
-						accessLevel: post.accessLevel,
-						category: post.category?.name || '',
-						title: post.title,
-						content: await parseAsText(post.content!),
-						createdAt: post.createdAt,
-					},
-				});
-			}
-		} catch (err) {
-			console.error(
-				`[scheduler] - [syncWithES] :: An error occurred: ${err}`
-			);
-		}
 	}
 }
