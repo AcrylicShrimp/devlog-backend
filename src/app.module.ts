d486752a4e514ea798322d990123e5ec89f5c3a0
AcrylicShrimp <led789zxpp@naver.com>
2021-01-25 00:21:21 +0900

    TEST: Test memory usage by removing some modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { AdminModule } from './admin/admin.module';
-import { DBModule } from './db/db.module';
-import { ESModule } from './es/es.module';
-import { ViewModule } from './view/view.module';
+// import { AdminModule } from './admin/admin.module';
+// import { DBModule } from './db/db.module';
+// import { ESModule } from './es/es.module';
+// import { ViewModule } from './view/view.module';
 
 @Module({
 	imports: [],
